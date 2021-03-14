3983c268fa62b0ecec4f400cf811e6185677a140
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2021-03-14 14:08:32 +0100

    t Remove redundant tests

--- src/test/kotlin/kata/tdd/GameOfLifeTest.kt
+++ src/test/kotlin/kata/tdd/GameOfLifeTest.kt
@@ -1,24 +1,9 @@
 package kata.tdd
 
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class GameOfLifeTest {
-
-    @Test
-    fun `Board accepts live cell coordinate`() {
-        Board(Pair(0,0))
-    }
-
-    @Test
-    fun `Board can list live cells`() {
-        val board = Board(Pair(0,0), Pair(1,1))
-
-        val liveCells = board.liveCells()
-
-        assertEquals(listOf(Pair(0,0), Pair(1,1)), liveCells)
-    }
 
     @Test
     fun `nextGeneration of no live cells is no live cells`() {
