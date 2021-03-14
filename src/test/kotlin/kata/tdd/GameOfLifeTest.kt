3b642d3f4d72217b85a4e9b57fc4f470f4c6a237
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2021-03-14 13:21:37 +0100

    F Cell accepts coordinates

--- src/test/kotlin/kata/tdd/GameOfLifeTest.kt
+++ src/test/kotlin/kata/tdd/GameOfLifeTest.kt
@@ -3,7 +3,7 @@ package kata.tdd
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
-internal class StuffTest {
+internal class GameOfLifeTest {
     @Test
     fun `test some method`() {
         assertTrue(true)
