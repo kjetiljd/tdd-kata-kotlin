fd20e9a1dd925f5655524343d4a96ccd458f2194
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2021-03-14 13:32:07 +0100

    F Introduce Board

--- src/main/kotlin/kata/tdd/GameOfLife.kt
+++ src/main/kotlin/kata/tdd/GameOfLife.kt
@@ -1,3 +1,9 @@
 package kata.tdd
 
 class Cell(val x: Int, val y: Int)
+
+class Board {
+    fun accept(liveCell: Cell) {
+
+    }
+}
--- src/test/kotlin/kata/tdd/GameOfLifeTest.kt
+++ src/test/kotlin/kata/tdd/GameOfLifeTest.kt
@@ -8,5 +8,9 @@ internal class GameOfLifeTest {
     fun `Cell accepts coordinates`() {
         Cell(0,0)
     }
-}
 
+    @Test
+    fun `Board accepts live cells`() {
+        Board().accept(Cell(0,0))
+    }
+}
