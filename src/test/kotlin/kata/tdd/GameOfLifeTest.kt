897021142d298378dfed0b600d9a5e85bbe062fa
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2021-03-14 14:11:39 +0100

    t board with one live cell will have no live cells nextGeneration

--- src/test/kotlin/kata/tdd/GameOfLifeTest.kt
+++ src/test/kotlin/kata/tdd/GameOfLifeTest.kt
@@ -1,5 +1,6 @@
 package kata.tdd
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -8,6 +9,15 @@ internal class GameOfLifeTest {
     @Test
     fun `nextGeneration of no live cells is no live cells`() {
         val board = Board()
+
+        val nextGeneration = board.nextGeneration()
+
+        assertTrue(nextGeneration.liveCells().isEmpty())
+    }
+
+    @Test
+    fun `board with one live cell will have no live cells nextGeneration`() {
+        val board = Board(Cell(0,0))
 
         val nextGeneration = board.nextGeneration()
 
