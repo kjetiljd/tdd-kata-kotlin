a2dc516a7a8053cb3f92e6f7ac93326753f3529a
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2021-03-14 14:30:18 +0100

    t Scenario with one neighbor

--- src/test/kotlin/kata/tdd/GameOfLifeTest.kt
+++ src/test/kotlin/kata/tdd/GameOfLifeTest.kt
@@ -1,6 +1,6 @@
 package kata.tdd
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -17,10 +17,54 @@ internal class GameOfLifeTest {
 
     @Test
     fun `board with one live cell will have no live cells nextGeneration`() {
-        val board = Board(Cell(0,0))
+        val board = Board(Cell(0, 0))
 
         val nextGeneration = board.nextGeneration()
 
         assertTrue(nextGeneration.liveCells().isEmpty())
     }
+
+    @Test
+    fun `Any live cell with fewer than two live neighbours dies`() {
+        val scenario = """
+            100
+            0x0
+            000
+            """
+        val board = Board(*scenario.liveCells)
+        val nextGeneration = board.nextGeneration()
+
+        assertFalse(
+            nextGeneration
+                .liveCells()
+                .contains(scenario.protagonist)
+        )
+    }
+
 }
+
+private val String.protagonist: Pair<Int, Int>
+    get() =
+        this.trimIndent()
+            .split("\n")
+            .mapIndexed { y, row ->
+                row.mapIndexed { x, cell ->
+                    if (cell == '1') Cell(x, y) else null
+                }
+            }
+            .flatten()
+            .filterNotNull()
+            .first()
+
+private val String.liveCells: Array<Pair<Int, Int>>
+    get() =
+        this.trimIndent()
+            .split("\n")
+            .mapIndexed { y, row ->
+                row.mapIndexed { x, cell ->
+                    if (cell == '1') Cell(x, y) else null
+                }
+            }
+            .flatten()
+            .filterNotNull()
+            .toTypedArray()
